e6bbfa4a0a40256325ef03565cb0b868f69c50b5
dohlee <dohlee.bioinfo@gmail.com>
2019-06-10 00:39:58 +0900

    Fetch test script from Github gist instead of having them in the repo.

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-for f in $(ls tests/*/startup.smk); do
-    # Execute each test in a subshell.
-    ( cd $(dirname $f); ./test.sh; );
-done
-
-echo "All tests exited with $?."
