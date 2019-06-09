a5d99059056bd7861d79a1baef9a616a0599a7f0
dohlee <dohlee.bioinfo@gmail.com>
2019-06-10 00:44:54 +0900

    Fix reproducibility test.

--- tests/test_reproducibility/test.sh
+++ tests/test_reproducibility/test.sh
@@ -9,7 +9,7 @@ snakemake -s ../../Snakefile --configfile config.yaml --config result_dir=result
 a=$(md5sum result0/test/test.motif1.homer_result | cut -d' ' -f1)
 b=$(md5sum result1/test/test.motif1.homer_result | cut -d' ' -f1)
 
-if [[ $a = $b ]]; then
+if [[ $a == $b ]]; then
     # Reproducible!
     curl https://gist.githubusercontent.com/dohlee/3ea154d52932b27d042566605a2cb9e2/raw/update_reproducibility.sh -H 'Cache-control: no-cache' | bash /dev/stdin -y
 else
